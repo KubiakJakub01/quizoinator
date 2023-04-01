2c408e8d9dd015b6ee37752b63874a7b5a8993cc
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-04-01 22:04:20 +0200

    Add sqlalchemy

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+from typing import List
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 app.secret_key = "mysecret"
@@ -9,6 +13,18 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
 
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    _id = Column("id", Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100))
+    email = Column(String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return f"User {self.name} with email {self.email}"
 
 @app.route("/")
 def home():
@@ -24,6 +40,9 @@ def user():
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            found_user = User.query.filter_by(name=user_name).first()
+            found_user.email = email
+            db.session.commit()
         else:
             if "email" in session:
                 email = session["email"]
@@ -39,9 +58,20 @@ def user():
 def login():
     if request.method == "POST":
         session.permanent = True
-        user = request.form["name"]
-        session["user"] = user
+        name = request.form["name"]
+        session["user"] = name
+        email = request.form["email"]
+        session["email"] = email
         flash("Login Successful!")
+        found_user = User.query.filter_by(name=name).first()
+        if found_user:
+            flash("User already exists!")
+            session["email"] = found_user.email
+        else:
+            usr = User(name=name, email=email)
+            db.session.add(usr)
+            db.session.commit()
+            flash("User created!")
         return redirect(url_for("user"))
     else:
         if "user" in session:
@@ -64,4 +94,6 @@ def admin():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
