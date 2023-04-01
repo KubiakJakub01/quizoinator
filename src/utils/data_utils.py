816dc2d4a217a7aa40327b8d81c5d50688cf032a
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-04-01 17:10:19 +0200

    Init User db table

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -3,12 +3,16 @@ Module for data utilities such as dataclasses sctructures saved in sqlalchemy da
 """
 from dataclasses import dataclass
 from typing import List
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 @dataclass
-class User:
-    id: int
-    name: str
-    email: str
+class User(SQLAlchemy.Model):
+    _id: Column("id", Integer, primary_key=True)
+    name: Column(String(100))
+    email: Column(String(100))
+
+    def __repr__(self):
+        return f"User {self.name} with email {self.email}"
