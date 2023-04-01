c68ebac2ebfb0c4d1e505126eb046733ab31e384
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-04-01 16:59:14 +0200

    Config flask sql-alchemy

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "mysecret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
 
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
