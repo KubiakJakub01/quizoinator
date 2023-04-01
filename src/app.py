392138b3f09475fb6a4f81a02ebddf0be6d1a64e
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-04-01 16:57:45 +0200

    Update navbar

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "mysecret"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 
 @app.route("/")
@@ -11,28 +13,44 @@ def home():
 
 @app.route("/user")
 def user():
+    email = None
     if "user" in session:
         user_name = session["user"]
-        return render_template("user.html", name=user_name, content=["HTML", "CSS", "JS"])
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template(
+            "user.html", name=user_name, email=email, content=["HTML", "CSS", "JS"]
+        )
     else:
+        flash("You are not logged in!")
         return redirect(url_for("login"))
 
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["name"]
         session["user"] = user
+        flash("Login Successful!")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash("Already logged in!")
             return redirect(url_for("user"))
         return render_template("login.html")
 
 
 @app.route("/logout")
 def logout():
+    flash(f"You have been logged out", "info")
     session.pop("user", None)
+    session.pop("email", None)
     return redirect(url_for("login"))
 
 
