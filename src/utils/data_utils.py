97c5ede1ee143634a5b4c672ba82ef9710ff9120
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-04-01 17:05:32 +0200

    Add User dataclass

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -5,3 +5,10 @@ from dataclasses import dataclass
 from typing import List
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+
+
+@dataclass
+class User:
+    id: int
+    name: str
+    email: str
