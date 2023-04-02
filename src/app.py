7242acb867807974d940804f66dbb4f230f79df3
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-04-02 16:24:24 +0200

    Update singup template

--- src/app.py
+++ src/app.py
@@ -6,6 +6,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 app = Flask(__name__)
 app.secret_key = "mysecret"
@@ -15,11 +18,12 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class Users(db.Model):
     _id = Column("id", Integer, primary_key=True, autoincrement=True)
-    name = Column(String(100))
-    email = Column(String(100))
+    name = Column(String(20), nullable=False, unique=True)
+    email = Column(String(20), nullable=False, unique=True)
     password_hash = Column(String(128))
+    data_created = Column(db.DateTime, default=db.func.current_timestamp())
     
     @property
     def password(self):
@@ -35,6 +39,14 @@ class User(db.Model):
     def __repr__(self):
         return f"User {self.name} with email {self.email}"
 
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), EqualTo("password2", message="Passwords must match")])
+    password2 = PasswordField("Confirm Password", validators=[DataRequired()])
+    submit = SubmitField("Sign Up")
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -49,7 +61,7 @@ def user():
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
-            found_user = User.query.filter_by(name=user_name).first()
+            found_user = Users.query.filter_by(name=user_name).first()
             found_user.email = email
             db.session.commit()
         else:
@@ -62,9 +74,33 @@ def user():
         flash("You are not logged in!")
         return redirect(url_for("login"))
 
+
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
-    pass
+    name = None
+    email = None
+    password = None
+    form = UserForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+        found_user = Users.query.filter_by(name=name).first()
+        if found_user:
+            flash("User already exists!")
+            return redirect(url_for("login"))
+        else:
+            usr = Users(name=name, email=email, password=password)
+            db.session.add(usr)
+            db.session.commit()
+            flash("User created!")
+            return redirect(url_for("login"))
+    return render_template("signup.html",
+                            name=name,
+                            email=email,
+                            password=password,
+                            form=form)
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -75,12 +111,12 @@ def login():
         email = request.form["email"]
         session["email"] = email
         flash("Login Successful!")
-        found_user = User.query.filter_by(name=name).first()
+        found_user = Users.query.filter_by(name=name).first()
         if found_user:
             flash("User already exists!")
             session["email"] = found_user.email
         else:
-            usr = User(name=name, email=email)
+            usr = Users(name=name, email=email)
             db.session.add(usr)
             db.session.commit()
             flash("User created!")
@@ -90,6 +126,10 @@ def login():
             flash("Already logged in!")
             return redirect(url_for("user"))
         return render_template("login.html")
+
+@app.route("/view")
+def view():
+    return render_template("view.html", users=Users.query.all())
 
 
 @app.route("/logout")
