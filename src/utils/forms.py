d420ae919a20665b412a3f11585e9f0c6ce32cb2
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-04-08 18:48:23 +0200

    Create post form

--- src/utils/forms.py
+++ src/utils/forms.py
@@ -4,6 +4,7 @@ Module for forms
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.widgets import TextArea
 
 
 class UserForm(FlaskForm):
@@ -28,3 +29,11 @@ class LoginForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log In")
+
+
+class PostForm(FlaskForm):
+    """Post form"""
+
+    title = StringField("Title", validators=[DataRequired()])
+    content = StringField("Content", validators=[DataRequired()], widget=TextArea()
+    submit = SubmitField("Submit")
