885384171ab532a37090530ac366227ba310ce41
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-04-10 14:07:44 +0200

    Move app module

--- app.py
+++ app.py
@@ -1,39 +1,17 @@
 """Main app file"""
-from datetime import timedelta
-
 # Import flask and template operators
-from flask import Flask, redirect, url_for, render_template, session, flash, request
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_ckeditor import CKEditor
+from flask import redirect, url_for, render_template, session, flash
+from flask_login import login_user, login_required, logout_user, current_user
 
-# Import SQLAlchemy
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Text, ForeignKey
-
-# Import password / encryption helper tools
-from werkzeug.security import generate_password_hash, check_password_hash
-
-# Import forms
-from utils.forms import UserForm, LoginForm, PostForm, SearchForm, AdminForm
-from utils.posts_utils import PostsUtils
-from utils.user_utils import UserUtils
-from utils.admin_utils import AdminUtils
-
-# Define the application configuration
-app = Flask(__name__)
-ckeditor = CKEditor(app)
-app.secret_key = "mysecret"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.permanent_session_lifetime = timedelta(minutes=5)
-
-# Define the database object which is imported
-db = SQLAlchemy(app)
- 
-# Define login manager
-login_menager = LoginManager()
-login_menager.init_app(app)
-login_menager.login_view = "login"
+# Import init
+from src import app, db, login_menager ,create_app, init_db, init_login_manager, init_ckeditor
+# Import models
+from src.models import Users, Posts, Admin
+# Import utils
+from src.utils.forms import UserForm, LoginForm, PostForm, SearchForm, AdminForm
+from src.utils.posts_utils import PostsUtils
+from src.utils.user_utils import UserUtils
+from src.utils.admin_utils import AdminUtils
 
 
 @login_menager.user_loader
@@ -185,68 +163,13 @@ def view_admins():
     id = current_user._id
     return admin_utils.view_admins(id)
 
-class Users(db.Model, UserMixin):
-    """User model for sqlalchemy database"""
-
-    _id = Column("id", Integer, primary_key=True, autoincrement=True)
-    name = Column(String(20), nullable=False, unique=True)
-    email = Column(String(20), nullable=False, unique=True)
-    password_hash = Column(String(128))
-    data_created = Column(db.DateTime, default=db.func.current_timestamp())
-    posts = db.relationship("Posts", backref="author", lazy=True)
-
-    @property
-    def password(self):
-        """Prevent password from being accessed"""
-        raise AttributeError("Password is not a readable attribute")
-
-    @password.setter
-    def password(self, password):
-        """Set password to a hashed password"""
-        self.password_hash = generate_password_hash(password)
-
-    def verify_password(self, password):
-        """Check if hashed password matches actual password"""
-        return check_password_hash(self.password_hash, password)
-
-    def get_id(self):
-        """Return id"""
-        return self._id
-
-    def __repr__(self):
-        return self.name
-
-
-class Posts(db.Model):
-    """Posts model for sqlalchemy database"""
-
-    _id = Column("id", Integer, primary_key=True, autoincrement=True)
-    title = Column(String(200), nullable=False, unique=True)
-    content = Column(Text, nullable=False, unique=True)
-    data_created = Column(db.DateTime, default=db.func.current_timestamp())
-    # Foreign key to Users
-    author_id = Column(Integer, ForeignKey("users.id"))
-
-    def __repr__(self):
-        return f"Post {self.title} with content {self.content}"
-
-
-class Admin(db.Model):
-    """Admin model for sqlalchemy database"""
-
-    admin_id = Column("id", Integer, primary_key=True, autoincrement=True)
-    # Foreign key to Users
-    user_id = Column(Integer, ForeignKey("users.id"))
-    # Foreign key to person who added admin
-    added_by = Column(Integer, ForeignKey("users.id"))
-    # reason for adding admin
-    reason = Column(String(200), nullable=False)
-    # Date admin was added
-    date_added = Column(db.DateTime, default=db.func.current_timestamp())
-
-
 
 if __name__ == "__main__":
+    app = create_app(app)
+    db = init_db(app)
+    login_menager = init_login_manager(app)
+    ckeditor = init_ckeditor(app)
+
     with app.app_context():
         db.create_all()
         admin_utils = AdminUtils(db, Admin,  Users, Posts, "admin")
